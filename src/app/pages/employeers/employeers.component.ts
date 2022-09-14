7f4f27e614dad96d45a581ba447136115f7e9342
KeniSV <u202018789@upc.edu.pe>
2022-09-14 18:25:17 -0500

    feature: creacion de listar empleados

--- src/app/pages/employeers/employeers.component.ts
+++ src/app/pages/employeers/employeers.component.ts
@@ -1,3 +1,6 @@
+import { EmployeesService } from './../../services/employees.service';
+import { Employer } from './../../models/Employer';
+import { MatTableDataSource } from '@angular/material/table';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
@@ -6,10 +9,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./employeers.component.css']
 })
 export class EmployeersComponent implements OnInit {
-
-  constructor() { }
+  dataSource: MatTableDataSource<Employer>=new MatTableDataSource();
+  displayedColumns: string[] = ['id', 'hotel', 'first_name', 'last_name', 'dni', 'email', 'photo', 'role'];
+  constructor(public Es:EmployeesService) { }
 
   ngOnInit(): void {
+    this.Es.mostrarEmpleado().subscribe((d)=>{
+      this.dataSource=new MatTableDataSource(d);
+    })
   }
 
 }
+
