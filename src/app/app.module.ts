f556a253f0525e643e93bd629116e0ae1c048317
LoelDev <pmpinzasr@hotmail.com>
2022-09-14 11:52:58 -0500

    feature: Agregado los pages necesarios para hacer los metodos listar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,17 +5,32 @@ import { AppRoutingModule } from './app-routing.module';
 //Material
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatIconModule } from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatListModule} from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatMenuModule } from '@angular/material/menu';
 //Componentes
 import { AppComponent } from './app.component';
 import { PrincipalComponent } from './pages/principal/principal.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { MyHotelComponent } from './pages/my-hotel/my-hotel.component';
+import { ReservationsComponent } from './pages/reservations/reservations.component';
+import { EmployeersComponent } from './pages/employeers/employeers.component';
+import { ResourcesListComponent } from './pages/resources-list/resources-list.component';
+import { ListHotelTasksComponent } from './pages/list-hotel-tasks/list-hotel-tasks.component';
 
 @NgModule({
-  declarations: [AppComponent, PrincipalComponent, NavBarComponent],
+  declarations: [
+    AppComponent,
+    PrincipalComponent,
+    NavBarComponent,
+    MyHotelComponent,
+    ReservationsComponent,
+    EmployeersComponent,
+    ResourcesListComponent,
+    ListHotelTasksComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -24,7 +39,8 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
     MatIconModule,
     MatButtonModule,
     MatSidenavModule,
-    MatListModule
+    MatListModule,
+    MatMenuModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
---
+++ src/app/pages/list-hotel-tasks/list-hotel-tasks.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-list-hotel-tasks',
+  templateUrl: './list-hotel-tasks.component.html',
+  styleUrls: ['./list-hotel-tasks.component.css']
+})
+export class ListHotelTasksComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
